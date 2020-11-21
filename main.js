974acc5c6d512455bf40aeee6fceb2669db3e49a
arv78 <arv78@ymail.com>
2020-11-21 19:16:59 +0330

    fourth commit - restrictions complete

--- main.js
+++ main.js
@@ -5,11 +5,43 @@ function isNumber(event) {
     return true;
 }
 
-function isPersian(event) {
-    if (event.keyCode >= 144 && event.keyCode <= 175 || event.keyCode >= 224 && event.keyCode <= 254) {
+function isNumber_(event) {
+    if ((event.keyCode >= 48 && event.keyCode <= 57) || event.keyCode == 47) {
         return true;
     }
     return false;
+}
+
+function yearcheck (date) {
+    var check = /13[1|9]0\/[0-1][1-9]\/[0-1][1-9]/;
+    if (!check.test(date)) {
+        return false;
+    }
+    return true;
+}
+
+function isall(event) {
+    if (event.keyCode < 32 || event.keyCode > 126) {
+        return false;
+    }
+    return true;
+}
+
+function isPassword(event) {
+    if (/^[a-zA-Z0-9_+!#$*=]*$/.test(event))
+  {
+    return true;
+  }
+    return false;
+}
+
+function isPersian(event){
+    var p = /^[\u0600-\u06FF\s]*$/;
+
+    if (!p.test(event)) {
+        return false;
+    }
+    return true;
 }
 
 function isEnglish(event) {
@@ -18,6 +50,15 @@ function isEnglish(event) {
     }
     return false;
 }
+
+function ValidateEmail(mail) {
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+  {
+    return true;
+  }
+    return false;
+}
+
 
 function validation() {
     var p = document.getElementsByName("uiPronoun")[0]
@@ -56,19 +97,33 @@ function validation() {
     var errorMessage = "";
     if (pro_noun == -1)
         errorMessage += " عنوان خود را انتخاب نمایید <br />"
+
     if (esm.length <3 || esm.length >50)
         errorMessage += "نام کاربر:تعداد کاراکتر ها باید بین 3 تا 50 باشد <br/>"
+    else if (!isPersian(esm)) {
+        errorMessage += "نام کاربر: فقط از کاراکتر های فارسی استفاده نمایید <br />"
+    }
+    
     if (esm_khan.length <3 || esm_khan.length >100)
         errorMessage += "نام خانوادگی کاربر:تعداد کاراکتر ها باید بین 3 تا 100 باشد <br/>"
-    if (name.length <1 || name.length >150)
+    else if (!isPersian(esm_khan)) {
+        errorMessage += "نام خانوادگی کاربر: فقط از کاراکتر های فارسی استفاده نمایید <br />"
+    }
+
+    if (name.length >150)
         errorMessage += "name: تعداد کاراکتر ها باید کمتر از 150 باشد <br/>"
-    if (surname.length <1 || surname.length >150)
+    if (surname.length >150)
         errorMessage += "surname: تعداد کاراکتر ها باید کمتر از 150 باشد <br/>"
 
     if (email == "") {
         errorMessage += "ایمیل خود را وارد نمایید<br />"
     }
-    else if (1==1){}
+    else if (email.length >64){
+        errorMessage += "ایمیل نا معتبر<br />"
+    }
+    else if (!ValidateEmail(email)){
+        errorMessage += "ایمیل نا معتبر<br />"
+    }
 
     if (phone == "") {
         errorMessage += "شماره موبایل خود را وارد نمایید<br />"
@@ -80,8 +135,29 @@ function validation() {
     if (ID_num.length != 10) {
         errorMessage += "شماره ملی  : تعداد رقم نا معتبر<br />"
     }
-    if (address.length >250) {
-        errorMessage += "آدرس طولانی می باشد"
+    if (!isPersian(address)) {
+        errorMessage += "آدرس: فقط از کاراکتر های فارسی استفاده نمایید <br />"
+    }
+    else if (address.length >250) {
+        errorMessage += "آدرس طولانی می باشد<br />"
+    }
+
+    if (selectedstatus == null) {
+        errorMessage += "وضعیت تاهل خود را مشخص نمایید<br />"
+    }
+    if (password.length <8 || password.length >24){
+        errorMessage += "تعداد کاراکتر نامعتبر <br />"
+    }
+    else if (!isPassword(password)) {
+        errorMessage += "پسورد نامعتبر <br />"
+    }
+    if (rep_password != password) {
+        errorMessage += "تکرار پسورد نا معتبر<br />"
+    }
+    if (B_date != "") {
+        if (!yearcheck(B_date)) {
+            errorMessage += "تاریخ نامعتبر<br />"
+        }
     }
 
     if (errorMessage != "") {
